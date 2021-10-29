82aefbfd957240fa86a2c53936df68cf40de8163
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-29 15:42:21 -0300

    Add btn to home in game page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,13 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
-import Door from '../components/Door'
-import { createDoors, refreshDoors } from '../functions/Doors'
-import DoorModel from '../model/DoorModel'
 import styles from '../styles/Home.module.css'
+
+import Link from "next//link"
 
 const Home: NextPage = () => {
 
-  const [doors,setDoors] = useState(createDoors(5,4))
-
-  function renderDoors(){
-    return doors.map((e:DoorModel)=>{
-      return <Door value={e} onChange={newDoor=> setDoors(refreshDoors(doors,newDoor))} key={e.doorNumber}/>
-    })
-  }
-
   return (
     <div className={styles.container}>
-      <div className={styles.doorsContainer}>
-        {renderDoors()}
-      </div>
+      <h1>Monty Hall The Game</h1>
     </div>
   )
 }
